da750efba47da4ae84f26f086d7bde17d3e1cc12
laseut <lase.ut@gmail.com>
2013-11-11 16:52:21 -0600

    changed update.sh to look for 'files changed'

--- update.sh
+++ update.sh
@@ -10,11 +10,14 @@ THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$THIS_DIR"
 
 S1="$(git pull origin)"
-S2="Already up-to-date."
-if [ "$S1" != "$S2" ] ; then
+if [[ "$S1" == *"files changed"* ]] ; then
+    echo "**************************************"
     echo "Changes detected. Updating database..."
+    echo "**************************************"
 elif [ "$1" = "-f" ] ; then
+    echo "*****************************"
     echo "Forcing update of database..."
+    echo "*****************************"
 else
     # No changed were made
     exit 0
@@ -26,10 +29,10 @@ echo "**************"
 echo "Running jabref"
 echo "**************"
 jabref -n -o update_database.sql,mysql publications.bib
-echo $?
+echo "Exit code: $?"
 echo "*************"
 echo "Running mysql"
 echo "*************"
 mysql --user=root --password=galliumnitride -D publications < update_database.sql
-echo $?
+echo "Exit code: $?"
 exit 0
