24039bdd391f644e5329d81b8e3a5dfa1003b5e6
laseut <lase.ut@gmail.com>
2013-11-11 15:30:35 -0600

    updated update.sh

--- update.sh
+++ update.sh
@@ -12,22 +12,25 @@ S2="Already up-to-date."
 echo $S1
 update=false
 if [ "$S1" != "$S2" ] ; then
-    update=true
     echo "Changes detected. Updating database..."
 elif [ "$1" = "-f" ] ; then
-    update=true
     echo "Forcing update of database..."
-fi
-if $update ; then
-    # Changes were made
-    jabref -n -o update_database.sql,mysql publications.bib
-    echo $?
-    mysql --user=root --password=galliumnitride -D publications < update_database.sql
-    echo $?
-    rm update_database.sql
-    exit 0
 else
     # No changed were made
     echo "No changes."
     exit 0
 fi
+
+# Changes were made
+rm update_database.sql
+echo "**************"
+echo "Running jabref"
+echo "**************"
+jabref -n -o update_database.sql,mysql publications.bib
+echo $?
+echo "*************"
+echo "Running mysql"
+echo "*************"
+mysql --user=root --password=galliumnitride -D publications < update_database.sql
+echo $?
+exit 0
